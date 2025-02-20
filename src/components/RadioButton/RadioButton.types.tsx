481cb38b175ff17907bc94a92978f9be2f90808f
rdogra-y <rdogra@academic.rrc.ca>
2025-02-19 21:56:43 -0600

    Added RadioButton component

---
+++ src/components/RadioButton/RadioButton.types.tsx
@@ -0,0 +1,9 @@
+export type RadioButtonProps = {
+    label: string;
+    value: string;
+    name: string;
+    checked?: boolean;
+    onChange: (value: string) => void;
+    disabled?: boolean;
+  };
+  
